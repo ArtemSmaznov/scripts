e35eb0138791fe7ce29427a3f974a32914b7798f
Artem Smaznov <ArtemSmaznov@gmail.com>
2023-08-07 17:43:15 -0400

    fix(lang): hyprland lang switching

--- get-lang.sh
+++ get-lang.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-setxkbmap -query | awk '$1=="layout:" {print $2}'
+if [[ $XDG_SESSION_TYPE == "x11" ]]; then
+    setxkbmap -query | awk '$1=="layout:" {print $2}'
+fi
+
+if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
+    # if [[ $XDG_DESKTOP_SESSION == "hyprland" ]]; then
+    # if [[ $XDG_SESSION_DESKTOP == "Hyprland" ]]; then
+    if [[ $XDG_CURRENT_DESKTOP == "Hyprland" ]]; then
+        hyprctl -j devices | jq -r '.keyboards[] | select(.name | contains("wlr")) .active_keymap'
+    fi
+fi
--- toggle-lang.sh
+++ toggle-lang.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
-case $($HOME/.local/bin/get-lang.sh) in
-    "us") new_lang="ru" ;;
-    "ru") new_lang="jp" ;;
-    "jp") new_lang="us" ;;
-    *)   new_lang="us" ;;
-esac
+if [[ $XDG_SESSION_TYPE == "x11" ]]; then
+    case $($HOME/.local/bin/get-lang.sh) in
+        "us") new_lang="ru" ;;
+        "ru") new_lang="jp" ;;
+        "jp") new_lang="us" ;;
+        *) new_lang="us" ;;
+    esac
 
-$HOME/.local/bin/set-lang.sh $new_lang
+    $HOME/.local/bin/set-lang.sh $new_lang
 
-if [[ $new_lang == 'jp' ]]
-then fcitx5-remote -o # enable japanese
-else fcitx5-remote -c # disable japanese
+    if [[ $new_lang == 'jp' ]]; then
+        fcitx5-remote -o # enable japanese
+    else
+        fcitx5-remote -c # disable japanese
+    fi
+fi
+
+if [[ $XDG_SESSION_TYPE == "wayland" ]]; then
+    # if [[ $XDG_DESKTOP_SESSION == "hyprland" ]]; then
+    # if [[ $XDG_SESSION_DESKTOP == "Hyprland" ]]; then
+    if [[ $XDG_CURRENT_DESKTOP == "Hyprland" ]]; then
+        case $($HOME/.local/bin/get-lang.sh) in
+            "Japanese") fcitx5-remote -o ;; # enable japanese
+            *) fcitx5-remote -c ;;          # disable japanese
+        esac
+    fi
 fi
