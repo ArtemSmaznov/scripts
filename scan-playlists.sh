a0c444f554aef9fe8eb053790715d6dbb4d2b80e
Artem Smaznov <ArtemSmaznov@gmail.com>
2023-09-04 19:48:24 -0400

    feat: scan-playlists.sh

---
+++ scan-playlists.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+music_dir="$XDG_MUSIC_DIR"
+playlists_dir="$XDG_CONFIG_HOME/mpd/playlists"
+
+cd "$music_dir" || exit 1
+
+echo "broken song links"
+for playlist in "$playlists_dir"/*; do
+    playlist=$(basename "$playlist" .m3u)
+
+    while read -r song; do
+        ls "$song" &>/dev/null || echo "$playlist: $song"
+    done <"$playlists_dir/$playlist.m3u"
+done
