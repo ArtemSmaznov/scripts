40c2b96cb4ad61bd4f1be0b10d7476d2e756b96e
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-02-19 16:04:44 -0500

    feat(get-video-thumbnail): add new script

---
+++ get-video-thumbnail.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+video_file="$1"
+size="$2"
+
+# variables
+#-------------------------------------------------------------------------------
+[ -z "$video_file" ] && echo "[ERROR] video file required as an argument" && exit 1
+[ -z "$size" ] && size=300
+
+# execution
+#===============================================================================
+ffmpeg \
+    -i "$video_file" \
+    -ss 00:00:05 \
+    -vframes 1 \
+    -vf "thumbnail,crop=min(iw\,ih):min(iw\,ih),scale=$size:$size" \
+    -loglevel quiet \
+    -y \
+    /tmp/thumbnail.jpg
