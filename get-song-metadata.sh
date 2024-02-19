a9caaa96435c9103fbd25db038443f9a6b04f942
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-02-19 18:36:58 -0500

    chore(get-song-metadata): generalize get-song-acoustid

---
+++ get-song-metadata.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+field="$1"
+music_file="$2"
+
+# execution
+#===============================================================================
+beet list "path:$music_file" \
+    -f "\$$field"
