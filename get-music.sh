0767b1b3db244ea7d2f1954b7a6ee09d921d4287
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-02-19 19:32:20 -0500

    feat(get-music): add file option

--- get-music.sh
+++ get-music.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env bash
-arg="$1"
+# environment variables
+#-------------------------------------------------------------------------------
+[ ! "$XDG_MUSIC_DIR" ] && export XDG_MUSIC_DIR="$HOME/Music"
 
 # variables
 #-------------------------------------------------------------------------------
 usage="""Usage:
-    get-music.sh artist
+    get-music.sh file
     get-music.sh albumcover
     get-music.sh albumcovercolor
-    get-music.sh album
-    get-music.sh title
+
     get-music.sh song
+    get-music.sh title
+    get-music.sh artist
+    get-music.sh album
+
     get-music.sh state
+    get-music.sh progress
+    get-music.sh volume
+
     get-music.sh flags
-    get-music.sh volume """
+    get-music.sh consume
+    get-music.sh crossfade """
 
 # functions
 #-------------------------------------------------------------------------------
@@ -37,8 +46,15 @@ convert_update () {
     fi
 }
 
+get_track_file () {
+    relative_file="$(mpc current -f %file%)"
+    absolute_file="$XDG_MUSIC_DIR/$relative_file"
+
+    echo "$absolute_file"
+}
+
 get_album_cover_file () {
-    music_track_dir="$(dirname "$XDG_MUSIC_DIR/$(mpc current -f %file%)")"
+    music_track_dir="$(dirname "$(get_track_file)")"
     album_cover_file=$(find "$music_track_dir" -type f -name "cover.*" | head -1)
     if [ -z "$album_cover_file" ]; then
         echo "$XDG_MUSIC_DIR/no-cover"
@@ -77,14 +93,15 @@ get_progress () {
 # execution
 #===============================================================================
 case $1 in
+    file) get_track_file ;;
     albumcover) get_album_cover_file ;;
     albumcovercolor) get_album_cover_color ;;
 
     song) mpc current -f "%artist% · %title%" ;;
 
-    artist) mpc current -f "%artist%" ;;
-    album) mpc current -f "%album%" ;;
     title) mpc current -f "%title%" ;;
+    album) mpc current -f "%album%" ;;
+    artist) mpc current -f "%artist%" ;;
 
     state) mpc status "%state%" ;;
     progress) get_progress ;;
