dcc1625d77eba11efdc178a87276c00e01dd5089
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-03-04 22:15:55 -0500

    feat(get-current-monitor): add hyprland

---
+++ get-current-monitor.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+[ "$1" ] && param=$1 || param=name
+
+# environment variables
+#-------------------------------------------------------------------------------
+[ ! "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="$HOME/.config"
+
+# variables
+#-------------------------------------------------------------------------------
+
+# functions
+#-------------------------------------------------------------------------------
+
+# setup
+#-------------------------------------------------------------------------------
+
+# execution
+#===============================================================================
+case $XDG_SESSION_TYPE in
+    wayland)
+        case $XDG_SESSION_DESKTOP in
+            Hyprland) "$XDG_CONFIG_HOME"/hypr/scripts/get-current-monitor.sh "$param" ;;
+            *) exit 1 ;;
+        esac ;;
+
+    *) exit 1 ;;
+esac
