ca9f5de611e1e5c336c6ac605dfa9825b7b0f118
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-03-17 22:25:35 -0400

    fix(get-ip): print last column

--- get-ip.sh
+++ get-ip.sh
@@ -2,9 +2,8 @@
 target="$1"
 
 if [[ ! $target ]]; then
-    # curl -s https://ipinfo.io/ip
     curl -s ifconfig.co
     exit
 fi
 
-host $target | awk '{ print $4 }'
+host $target | awk '{ print $NF }'
