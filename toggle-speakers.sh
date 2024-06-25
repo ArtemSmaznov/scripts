aad963fb742f12525ebe8a87c3eb8d08c50c37e3
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-06-24 20:39:16 -0400

    feat: add toggle-speakers

---
+++ toggle-speakers.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# variables --------------------------------------------------------------------
+device_name="google-home"
+
+# functions --------------------------------------------------------------------
+get_device_mac () {
+    device_mac="$1"
+    bluetoothctl devices |
+        awk '$3 == "'"$device_mac"'" { print $2 }'
+}
+
+is_device_blocked () {
+    device_mac="$1"
+    bluetoothctl info "$device_mac" |
+        awk '$1 == "Blocked:" { print $2 }'
+}
+
+toggle_action () {
+    device_blocked="$1"
+    [ "$device_blocked" == "yes" ] &&
+        action="unblock" ||
+            action="block"
+
+    echo "$action"
+}
+
+# setup ------------------------------------------------------------------------
+device_mac=$(get_device_mac "$device_name")
+device_blocked=$(is_device_blocked "$device_mac")
+
+# execution ====================================================================
+case $device_blocked in
+    no)
+        bluetoothctl block "$device_mac"
+    ;;
+    yes)
+        bluetoothctl unblock "$device_mac"
+        bluetoothctl connect "$device_mac"
+    ;;
+esac
