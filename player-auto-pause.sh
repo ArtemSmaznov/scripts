a86f0a2c01232da5aee13c5f63bf1951f72fe68a
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-07-15 20:37:42 -0400

    fix(auto-pause): don't pause on switch to headphones

--- player-auto-pause.sh
+++ player-auto-pause.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 # functions --------------------------------------------------------------------
 function get_all_players {
-    playerctl --list-all
+    blacklist=(
+        "momentum_4"
+    )
+    blacklist_concatted="${blacklist[*]}"
+    blacklist_formatted="${blacklist_concatted/ /,}"
+
+    playerctl --list-all --ignore-player "$blacklist_formatted"
 }
 
 function get_current_player {
-    playerctl --list-all | head -1
+    get_all_players | head -1
 }
 
 function get_player_state {
@@ -15,9 +21,8 @@ function get_player_state {
 
 function pause_players_except {
     new_player="$1"
-    players=$(get_all_players)
 
-    for player in $players; do
+    for player in $(get_all_players); do
         if [ "$player" != "$new_player" ]; then
             pause_player "$player"
         fi
@@ -32,14 +37,16 @@ function pause_player {
 }
 
 last_player=""
+new_player=$(get_current_player)
 
 # execution ====================================================================
 while true; do
     new_player=$(get_current_player)
-    new_player_state=$(get_player_state $new_player)
+    new_player_state=$(get_player_state "$new_player")
 
     if [[ "$new_player" != "$last_player" && "$new_player_state" == "Playing" ]]; then
         last_player="$new_player"
+        echo "[INFO] switching to player: $new_player"
         pause_players_except "$new_player"
     fi
 
