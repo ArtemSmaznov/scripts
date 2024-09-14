a96b9b30905a97fff6d68d44b99f5aaefec19c52
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-09-14 11:22:20 -0400

    feat(updates): add icon for pacman

--- get-update-flags.sh
+++ get-update-flags.sh
@@ -3,10 +3,11 @@
 updates_file="/var/cache/pacman/updates"
 
 # setup ------------------------------------------------------------------------
-grep -q "wine"      "$updates_file" && flag+=wine-bottle
-grep -q "mesa"      "$updates_file" && flag+=display
 grep -q "linux-zen" "$updates_file" && flag+=
-grep -q "hyprland"  "$updates_file" && flag+=droplet
+grep -q "mesa" "$updates_file" && flag+=display
+grep -q "pacman" "$updates_file" && flag+=ghost
+grep -q "hyprland" "$updates_file" && flag+=droplet
+grep -q "wine" "$updates_file" && flag+=wine-bottle
 
 # execution ====================================================================
 echo "$flag"
