3280ed0df85701441bd5483ca30826a407bfa53c
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-10-06 00:14:03 -0400

    chore(playerctl): increase volume step 2 -> 5

--- playerctl.sh
+++ playerctl.sh
@@ -3,11 +3,11 @@
 
 # execution ====================================================================
 case $1 in
-    toggle)    playerctl play-pause   ;;
-    stop)      playerctl stop         ;;
-    prev)      playerctl previous     ;;
-    next)      playerctl next         ;;
+toggle) playerctl play-pause ;;
+stop) playerctl stop ;;
+prev) playerctl previous ;;
+next) playerctl next ;;
 
-    vol-up)    playerctl volume 0.02+ ;;
-    vol-down)  playerctl volume 0.02- ;;
+vol-up) playerctl volume 0.05+ ;;
+vol-down) playerctl volume 0.05- ;;
 esac
