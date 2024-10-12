523123df35aed70e8689660911c7e7079d1cf029
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-10-12 13:39:14 -0400

    refactor(updates): add script args

--- get-updates.sh
+++ get-updates.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
-# variables --------------------------------------------------------------------
+# variables ====================================================================
 updates_file="/var/cache/pacman/updates"
+[ ! -f "$updates_file" ] && echo "[ERROR] $updates_file is missing!" && exit 1
 
-# setup ------------------------------------------------------------------------
-if [ ! -f "$updates_file" ]; then
-    echo "Error: $updates_file is missing!"
-    exit 1
-fi
+# options ----------------------------------------------------------------------
 
-# execution ====================================================================
-wc -l <"$updates_file"
+# functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function get_count() {
+    wc -l <"$updates_file"
+}
+
+# setup ________________________________________________________________________
+while getopts "jh:" opt; do
+    case "$opt" in
+    j) get_count ;;
+    *) get_count ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+# execution ********************************************************************
+get_count
